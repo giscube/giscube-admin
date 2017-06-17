3a9b5675704f4918a9c49ec724f6d6e31b2fb7b9
abusquets <abusquets@gmail.com>
2017-06-17 14:01:24 +0200

    Add WMS Url to admin gis server

--- qgisserver/admin.py
+++ qgisserver/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from django.conf import settings
 
 from qgisserver.models import Service
 
 
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'url_wms')
     exclude = ('service_path',)
     search_fields = ('name', 'title',)
+
+    def url_wms(self, obj):
+        return '<a target="_blank" href="%s/qgisserver/services/%s">WMS URL %s</a>' % (
+            settings.GISCUBE_URL, obj.name, obj.name)
+    url_wms.allow_tags = True
+    url_wms.short_description = 'WMS URL'
+
 
 admin.site.register(Service, ServiceAdmin)
