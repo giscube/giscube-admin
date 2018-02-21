469f2326248797eccc4b051a2bdd61309f77e682
Manel Clos <manelclos@gmail.com>
2018-02-21 11:16:30 +0100

    update urls.py

--- geoportal/urls.py
+++ geoportal/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
 from geoportal.views import GeoportalHomeView, GeoportalSearchView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', GeoportalHomeView.as_view(), name='home'),
     # url(r'^search/', include('haystack.urls')),
     url(r'^search/', GeoportalSearchView.as_view(), name='search'),
-)
+]
