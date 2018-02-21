471fdc13d58adc5c52a9e706a5306030ca3b434b
Manel Clos <manelclos@gmail.com>
2018-02-21 10:10:49 +0100

    db configuration only from environment

--- giscube/settings.py
+++ giscube/settings.py
@@ -127,12 +127,12 @@ WSGI_APPLICATION = 'giscube.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': os.environ.get('DB_NAME', 'giscube'),
-         'USER': os.environ.get('DB_USER', 'giscube'),
-         'PASSWORD': os.environ.get('DB_PASSWORD', 'giscube'),
-         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-         'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     },
 }
 
