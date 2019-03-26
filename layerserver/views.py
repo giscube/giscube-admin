2ead7a76b875271875ef328297dcdaa704f04bbb
abusquets <abusquets@gmail.com>
2019-03-26 18:12:47 +0100

    Fix GeoJsonLayer EndPoint

--- layerserver/views.py
+++ layerserver/views.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.core.files.uploadedfile import UploadedFile
 from django.forms.models import model_to_dict
+from django.shortcuts import get_object_or_404
 from django.utils.functional import cached_property
 
 from rest_framework import filters, parsers, status, views, viewsets
@@ -47,7 +48,7 @@ def GeoJSONLayerView(request, layer_name):
     # FIXME: why is this needed?
     # layer_name = ''.join(layer_name.split('.')[:-1])
     layer = get_object_or_404(GeoJsonLayer, name=layer_name, active=True)
-    if service.visibility == 'private' and not request.user.is_authenticated:
+    if layer.visibility == 'private' and not request.user.is_authenticated:
         return HttpResponseForbidden()
     if layer.visibility == 'private' and not request.user.is_authenticated:
         return HttpResponseForbidden()
