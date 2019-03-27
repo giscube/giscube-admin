657236c1a8afc3ff92889cbad891a1976c18c98d
abusquets <abusquets@gmail.com>
2019-03-27 16:48:04 +0100

    Use autocomplete for category and set order

--- giscube/admin.py
+++ giscube/admin.py
@@ -2,6 +2,7 @@
 
 
 from django.contrib import admin
+from django.db.models import F
 
 from django_vue_tabs.admin import TabsMixin
 
@@ -14,8 +15,19 @@ admin.site.site_header = 'GISCube'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('parent',)
     list_display = ('name', 'parent', '__str__')
     search_fields = ('name', 'parent__name')
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by(F('parent__name').desc(nulls_last=False), 'name')
+        return queryset, use_distinct
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.order_by(F('parent__name').desc(nulls_last=False), 'name')
+        return qs
 
 
 @admin.register(DBConnection)
