d60cd443830259280f3eb0f46a6be0acb6d79f8c
abusquets <abusquets@gmail.com>
2019-04-11 15:34:57 +0200

    Add list to GeoJsonLayer API

--- layerserver/urls.py
+++ layerserver/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, re_path
 
 from .views import (
-    GeoJSONLayerView, DBLayerViewSet, DBLayerDetailViewSet,
-    DBLayerContentViewSet, DBLayerContentBulkViewSet
-)
+    DBLayerDetailViewSet, DBLayerContentViewSet, DBLayerContentBulkViewSet, DBLayerViewSet, GeoJSONLayerViewSet)
 
+
+geojsonlayer_list = GeoJSONLayerViewSet.as_view({
+    'get': 'list',
+})
+
+geojsonlayer_detail = GeoJSONLayerViewSet.as_view({
+    'get': 'retrieve',
+})
 
 layer_list = DBLayerViewSet.as_view({
     'get': 'list',
@@ -29,8 +35,9 @@ content_detail = DBLayerContentViewSet.as_view({
 content_bulk = DBLayerContentBulkViewSet.as_view()
 
 urlpatterns = [
-    re_path(r'^geojsonlayers/(?P<layer_name>[-\w]{1,255})?(\.json|\.geojson)?$',
-            GeoJSONLayerView, name='geojsonlayer'),
+    path('geojsonlayers/', geojsonlayer_list, name='geojsonlayer'),
+    re_path(r'^geojsonlayers/(?P<name>[-\w]{1,255})?(\.json|\.geojson)?$',
+            geojsonlayer_detail, name='geojsonlayer'),
     path('databaselayers/<slug:layer_slug>/data/<str:pk>/<str:attribute>/thumbnail/<path:path>',
          DBLayerContentViewSet.as_view({'get': 'thumbnail_value'}), name='content-detail-thumbnail-value'),
     path('databaselayers/<slug:layer_slug>/data/<str:pk>/<str:attribute>/<path:path>',
