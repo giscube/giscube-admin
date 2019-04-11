8c3aa9e9befb9328b891cb3faef3f62f7c2dd64f
abusquets <abusquets@gmail.com>
2019-04-11 16:24:10 +0200

    Rename views to api

--- layerserver/urls.py
+++ layerserver/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from .views import (
+from .api import (
     DBLayerDetailViewSet, DBLayerContentViewSet, DBLayerContentBulkViewSet, DBLayerViewSet, GeoJSONLayerViewSet)
 
 
