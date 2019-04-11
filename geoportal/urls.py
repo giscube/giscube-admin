f09ac06b58bfaaf46002f08854870d05f17c3010
abusquets <abusquets@gmail.com>
2019-04-11 12:21:27 +0200

    Add category to search index

--- geoportal/urls.py
+++ geoportal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.GeoportalHomeView.as_view(), name='home'),
     path('search/', views.GeoportalSearchView.as_view(), name='search'),
     path('catalog/', views.GeoportalCatalogView.as_view(), name='catalog'),
+    path('category/', views.GeoportalCategoryView.as_view(), name='catalog'),
 ]
