f436ca6c9c3fd34347ad4792d162949ae5ab3696
abusquets <abusquets@gmail.com>
2019-04-16 18:23:59 +0200

    Add style rules

--- layerserver/models_mixins.py
+++ layerserver/models_mixins.py
@@ -37,24 +37,28 @@ class StyleMixin(models.Model):
     """
     Style mixin.
     """
-    shapetype = models.CharField(blank=True, null=True, max_length=20,
-                                 choices=SHAPE_TYPES_CHOICES)
-    shape_radius = models.IntegerField(blank=True, null=True)
-    stroke_color = models.CharField(
-        max_length=50, blank=True, null=True,
-        default=settings.LAYERSERVER_STYLE_STROKE_COLOR)
-    stroke_width = models.IntegerField(blank=True, null=True, default=1)
-    stroke_dash_array = models.CharField(max_length=25, blank=True, null=True,
-                                         default='')
-    fill_color = models.CharField(
-        max_length=50, blank=True, null=True,
-        default=settings.LAYERSERVER_STYLE_FILL_COLOR)
-    fill_opacity = models.DecimalField(blank=True, null=True, default=1,
-                                       max_digits=2, decimal_places=1)
+    shape_radius = models.CharField(max_length=50, blank=True, null=True)
+    stroke_color = models.CharField(max_length=50, blank=True, null=True,
+                                    default=settings.LAYERSERVER_STYLE_STROKE_COLOR)
+    stroke_width = models.CharField(max_length=50, blank=True, null=True, default='1')
+    stroke_dash_array = models.CharField(max_length=50, blank=True, null=True, default='')
+    fill_color = models.CharField(max_length=50, blank=True, null=True,
+                                  default=settings.LAYERSERVER_STYLE_FILL_COLOR)
+    fill_opacity = models.CharField(max_length=50, blank=True, null=True, default='1')
     marker_color = models.CharField(max_length=50, blank=True, null=True)
     icon_type = models.CharField(max_length=100, blank=True, null=True, choices=(('fa', 'fa',), ('img', 'img',)))
     icon = models.CharField(max_length=255, blank=True, null=True)
     icon_color = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class ShapeStyleMixin(StyleMixin):
+    """
+    Style mixin.
+    """
+    shapetype = models.CharField(blank=True, null=True, max_length=20, choices=SHAPE_TYPES_CHOICES)
 
     class Meta:
         abstract = True
