e33e9c8bf567cd101b83847329d3abb60177c738
abusquets <abusquets@gmail.com>
2019-04-24 08:30:50 +0200

    Fix geoportal urls name

--- geoportal/urls.py
+++ geoportal/urls.py
@@ -4,7 +4,7 @@ from geoportal import views
 
 urlpatterns = [
     path('', views.GeoportalHomeView.as_view(), name='home'),
-    path('search/', views.GeoportalSearchView.as_view(), name='search'),
-    path('catalog/', views.GeoportalCatalogView.as_view(), name='catalog'),
-    path('category/', views.GeoportalCategoryView.as_view(), name='catalog'),
+    path('search/', views.GeoportalSearchView.as_view(), name='geoportal-search'),
+    path('catalog/', views.GeoportalCatalogView.as_view(), name='geoportal-catalog'),
+    path('category/', views.GeoportalCategoryView.as_view(), name='geoportal-category'),
 ]
