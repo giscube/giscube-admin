f9e55e36515e8777436b01dc9a383a81d1554084
abusquets <abusquets@gmail.com>
2019-05-07 11:27:43 +0200

    Remove old home

--- geoportal/urls.py
+++ geoportal/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from geoportal import views
 
 urlpatterns = [
-    path('', views.GeoportalHomeView.as_view(), name='home'),
     path('search/', views.GeoportalSearchView.as_view(), name='geoportal-search'),
     path('catalog/', views.GeoportalCatalogView.as_view(), name='geoportal-catalog'),
     path('category/', views.GeoportalCategoryView.as_view(), name='geoportal-category'),
