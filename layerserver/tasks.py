07f5c26fe1d12cf1010c65c6048004b4f398a8f8
abusquets <abusquets@gmail.com>
2019-05-10 12:34:45 +0200

    Add checkbox to request refresh data in GeoJSONLayers

--- layerserver/tasks.py
+++ layerserver/tasks.py
@@ -2,6 +2,6 @@ from giscube.celery import app
 
 
 @app.task()
-def async_geojsonlayer_refresh(pk):
+def async_geojsonlayer_refresh(pk, force_refresh_data_file):
     from layerserver.utils import geojsonlayer_refresh
-    geojsonlayer_refresh(pk)
+    geojsonlayer_refresh(pk, force_refresh_data_file)
