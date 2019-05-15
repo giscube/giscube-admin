ec4fafae548e2cfac6cb4b5b10791c4a9cd43aa9
abusquets <abusquets@gmail.com>
2019-05-15 22:35:55 +0200

    Add Datetime widget

---
+++ layerserver/widgets/datetime.py
@@ -0,0 +1,36 @@
+import inspect
+import json
+
+from django.utils.translation import gettext as _
+
+from .base import BaseJSONWidget
+
+
+class DatetimeWidget(BaseJSONWidget):
+    TEMPLATE = inspect.cleandoc("""
+    {
+        "format": "YYYY-MM-DD HH:mm:ss"
+    }
+    """)
+
+    ERROR_FORMAT_REQUIRED = _('\'format\' attribute is required')
+
+    @staticmethod
+    def is_valid(value):
+        try:
+            data = json.loads(value)
+        except Exception:
+            return DatetimeWidget.ERROR_INVALID_JSON
+
+        if 'format' not in data:
+            return DatetimeWidget.ERROR_FORMAT_REQUIRED
+
+    @staticmethod
+    def serialize_widget_options(obj):
+        try:
+            options = json.loads(obj.widget_options)
+        except Exception:
+            return {'error': 'ERROR PARSING WIDGET OPTIONS'}
+
+        data = {'widget_options': options}
+        return data
