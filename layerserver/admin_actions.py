55c56a925c354b711c34f1f81ac53253f097d8cb
abusquets <abusquets@gmail.com>
2019-05-16 15:45:09 +0200

    Action to force geojsonlayers refresh

---
+++ layerserver/admin_actions.py
@@ -0,0 +1,18 @@
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
+from django.utils.translation import ugettext as _
+
+from .tasks import async_geojsonlayer_refresh
+
+
+def geojsonlayer_force_refresh_data(modeladmin, request, queryset):
+    if not request.user.has_perm('layerserver.change_geojsonlayer'):
+        raise PermissionDenied
+
+    for obj in queryset.all():
+        async_geojsonlayer_refresh.delay(obj.pk, True)
+    n = queryset.count()
+    modeladmin.message_user(request, _('Forcing refresh of %s geojsonlayers') % n, messages.INFO)
+
+
+geojsonlayer_force_refresh_data.short_description = _('Force refresh data files')
