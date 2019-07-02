efca8efc0543ffd3cd0ac2d00b206c3077a1f860
Alexandre <abusquets@gmail.com>
2019-07-02 14:45:17 +0200

    Add missing translations

--- layerserver/models_mixins.py
+++ layerserver/models_mixins.py
@@ -19,13 +19,13 @@ class BaseLayerMixin(models.Model):
     BaseLayer mixin.
     """
     category = models.ForeignKey(
-        Category, null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=50, unique=True)
-    title = models.CharField(max_length=100, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    keywords = models.CharField(max_length=200, null=True, blank=True)
-    active = models.BooleanField(default=True, help_text='Enable/disable usage')
-    visible_on_geoportal = models.BooleanField(default=False)
+        Category, verbose_name=_('category'), null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(_('name'), max_length=50, unique=True)
+    title = models.CharField(_('title'), max_length=100, null=True, blank=True)
+    description = models.TextField(_('description'), null=True, blank=True)
+    keywords = models.CharField(_('keywords'), max_length=200, null=True, blank=True)
+    active = models.BooleanField(_('active'), default=True, help_text='Enable/disable usage')
+    visible_on_geoportal = models.BooleanField(_('visible on geoportal'), default=False)
 
     class Meta:
         abstract = True
@@ -35,18 +35,19 @@ class StyleMixin(models.Model):
     """
     Style mixin.
     """
-    shape_radius = models.CharField(max_length=50, blank=True, null=True)
-    stroke_color = models.CharField(max_length=50, blank=True, null=True,
+    shape_radius = models.CharField(_('shape radius'), max_length=50, blank=True, null=True)
+    stroke_color = models.CharField(_('stroke color'), max_length=50, blank=True, null=True,
                                     default=settings.LAYERSERVER_STYLE_STROKE_COLOR)
-    stroke_width = models.CharField(max_length=50, blank=True, null=True, default='1')
-    stroke_dash_array = models.CharField(max_length=50, blank=True, null=True, default='')
-    fill_color = models.CharField(max_length=50, blank=True, null=True,
+    stroke_width = models.CharField(_('stroke width'), max_length=50, blank=True, null=True, default='1')
+    stroke_dash_array = models.CharField(_('stroke dash array'), max_length=50, blank=True, null=True, default='')
+    fill_color = models.CharField(_('fill color'), max_length=50, blank=True, null=True,
                                   default=settings.LAYERSERVER_STYLE_FILL_COLOR)
-    fill_opacity = models.CharField(max_length=50, blank=True, null=True, default='1')
-    marker_color = models.CharField(max_length=50, blank=True, null=True)
-    icon_type = models.CharField(max_length=100, blank=True, null=True, choices=(('fa', 'fa',), ('img', 'img',)))
-    icon = models.CharField(max_length=255, blank=True, null=True)
-    icon_color = models.CharField(max_length=50, blank=True, null=True)
+    fill_opacity = models.CharField(_('fill opacity'), max_length=50, blank=True, null=True, default='1')
+    marker_color = models.CharField(_('marker color'), max_length=50, blank=True, null=True)
+    icon_type = models.CharField(_('icon type'), max_length=100, blank=True, null=True,
+                                 choices=(('fa', 'fa',), ('img', 'img',)))
+    icon = models.CharField(_('icon'), max_length=255, blank=True, null=True)
+    icon_color = models.CharField(_('icon color'), max_length=50, blank=True, null=True)
 
     class Meta:
         abstract = True
@@ -66,7 +67,8 @@ class PopupMixin(models.Model):
     """
     Popup mixin.
     """
-    popup = models.TextField(blank=True, null=True)
+    help_text = '%s %s' % (_('Field between curly braces. e.g.'), '{%s}' % _('street'))
+    popup = models.TextField(_('popup'), blank=True, null=True, help_text=help_text)
 
     def get_default_popup_content(self, fields):
         content = []
@@ -86,7 +88,8 @@ class TooltipMixin(models.Model):
     """
     Tooltip mixin.
     """
-    tooltip = models.TextField(_('tooltip'), blank=True, null=True)
+    help_text = '%s %s' % (_('Field between curly braces. e.g.'), '{%s}' % _('street'))
+    tooltip = models.TextField(_('tooltip'), blank=True, null=True, help_text=help_text)
 
     class Meta:
         abstract = True
