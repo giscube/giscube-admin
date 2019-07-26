4bd55064ff36cc2cb752b35088a474a9711538ae
abusquets <abusquets@gmail.com>
2019-07-26 13:57:58 +0200

    version 0.109

--- giscube/__init__.py
+++ giscube/__init__.py
@@ -1,6 +1,6 @@
 from .utils import get_version  # noqa isort:skip
 
 # 'alpha', 'beta', 'rc', 'final'
-VERSION = (0, 108, 0, 'final', 0)
+VERSION = (0, 109, 0, 'final', 0)
 
 __version__ = get_version(VERSION)
