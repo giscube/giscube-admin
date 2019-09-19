734b3bd42367b2009fcfeaf50418105388448d5a
abusquets <abusquets@gmail.com>
2019-09-19 12:09:29 +0200

    Add autocomplete to references service

--- qgisserver/admin.py
+++ qgisserver/admin.py
@@ -1,5 +1,8 @@
+from django.db.models import Value as V
+from django.db.models.functions import Coalesce, Concat
 from django.conf import settings
 from django.contrib import admin
+from django.urls import resolve
 from django.utils.html import format_html
 
 from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter
@@ -22,6 +25,22 @@ class ServiceAdmin(admin.ModelAdmin):
     exclude = ('service_path', 'active')
     search_fields = ('name', 'title', 'keywords')
     filter_horizontal = ('servers',)
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        current_url = resolve(request.path_info).url_name
+        if current_url == 'qgisserver_service_autocomplete':
+            queryset = queryset.annotate(custom_order=Concat('title', 'name'))
+            queryset = queryset.order_by('custom_order')
+        return queryset, use_distinct
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        current_url = resolve(request.path_info).url_name
+        if current_url == 'qgisserver_service_autocomplete':
+            queryset = queryset.annotate(custom_order=Concat(Coalesce('title', V('')), 'name'))
+            queryset = queryset.order_by('custom_order')
+        return queryset
 
     def url_wms(self, obj):
         url = url_slash_join(settings.GISCUBE_URL, '/qgisserver/services/%s' % obj.name)
