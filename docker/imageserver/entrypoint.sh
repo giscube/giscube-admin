2cfcb956004f50d9eed405723bae01576b110dd2
Alexandre <abusquets@gmail.com>
2019-10-14 14:31:59 +0200

    Fix DOCKER_USER

--- docker/imageserver/entrypoint.sh
+++ docker/imageserver/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-DOCKER_DATA=/docker_data
-DOCKER_USER=www-data
-
-[ $(stat -c %U "$DOCKER_DATA") == "$DOCKER_USER" ] || chown -R "$DOCKER_USER" "$DOCKER_DATA"
-
 exec /usr/sbin/gosu "$DOCKER_USER" "$@"
