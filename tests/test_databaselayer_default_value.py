e2f18312e653d191f50253a13fc2b1f91cee6ffc
Alexandre <abusquets@gmail.com>
2019-10-25 12:07:30 +0200

    Fixes

--- tests/test_databaselayer_default_value.py
+++ tests/test_databaselayer_default_value.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.urls import reverse
 
 from giscube.models import DBConnection
-from layerserver.model_legacy import ModelFactory
+from layerserver.model_legacy import create_dblayer_model
 from layerserver.models import DataBaseLayer
 from tests.common import BaseTest
 
@@ -20,7 +20,9 @@ class DataBaseLayerDefaultValueTestCase(BaseTest):
         conn.port = settings.DATABASES['default']['PORT']
         conn.save()
         self.conn = conn
+        self.sql_to_delete = []
 
+    def test_sequence(self):
         sql = """
             CREATE SEQUENCE tree_code_seq
                 START WITH 1
@@ -36,11 +38,16 @@ class DataBaseLayerDefaultValueTestCase(BaseTest):
                 description character varying(250)
             );
         """
-        with conn.get_connection().cursor() as cursor:
+        with self.conn.get_connection().cursor() as cursor:
             cursor.execute(sql)
 
+        self.sql_to_delete = [
+            'DROP TABLE tree;',
+            'DROP SEQUENCE tree_code_seq;',
+        ]
+
         layer = DataBaseLayer()
-        layer.db_connection = conn
+        layer.db_connection = self.conn
         layer.slug = 'tree'
         layer.name = 'tree'
         layer.table = 'tree'
@@ -56,14 +63,12 @@ class DataBaseLayerDefaultValueTestCase(BaseTest):
         field.blank = True
         field.save()
 
-    def test_sequence(self):
-        with ModelFactory(self.layer) as Tree:
-            obj = Tree()
-            obj.description = 'test 1'
-            obj.save()
+        Tree = create_dblayer_model(self.layer)
+        obj = Tree()
+        obj.description = 'test 1'
+        obj.save()
 
-            obj.refresh_from_db()
-            self.assertEqual(obj.code, '1')
+        self.assertEqual(Tree.objects.last().code, '1')
 
         data = {
             'ADD': [
@@ -78,12 +83,73 @@ class DataBaseLayerDefaultValueTestCase(BaseTest):
         url = reverse('content-bulk', kwargs={'name': self.layer.name})
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 204)
-        with ModelFactory(self.layer) as Tree:
-            self.assertEqual(Tree.objects.last().code, '2')
+        # with ModelFactory(self.layer) as Tree:
+        self.assertEqual(Tree.objects.last().code, '2')
+
+    def test_sequence_int(self):
+        sql = """
+            CREATE SEQUENCE tree_int_code_seq
+                START WITH 1
+                INCREMENT BY 1
+                NO MINVALUE
+                NO MAXVALUE
+                CACHE 1;
+            CREATE TABLE tree_int (
+                id SERIAL NOT NULL,
+                code integer NOT NULL
+                    default (nextval('tree_int_code_seq'::regclass)),
+                name character varying(12) default 'Hell")o',
+                description character varying(250)
+            );
+        """
+        with self.conn.get_connection().cursor() as cursor:
+            cursor.execute(sql)
+        self.sql_to_delete = [
+            'DROP TABLE tree_int;',
+            'DROP SEQUENCE tree_int_code_seq;',
+        ]
+        layer = DataBaseLayer()
+        layer.db_connection = self.conn
+        layer.slug = 'tree_int'
+        layer.name = 'tree_int'
+        layer.table = 'tree_int'
+        layer.pk_field = 'id'
+        layer.anonymous_view = True
+        layer.anonymous_add = True
+        layer.anonymous_update = True
+        layer.anonymous_delete = True
+        layer.save()
+        self.layer = layer
+
+        field = self.layer.fields.filter(name='code').first()
+        field.blank = True
+        field.save()
+
+        Tree = create_dblayer_model(self.layer)
+        obj = Tree()
+        obj.description = 'test 1'
+        obj.save()
+
+        self.assertEqual(Tree.objects.last().code, 1)
+
+        data = {
+            'ADD': [
+                {
+                    'description': 'test 2'
+                },
+            ],
+            'UPDATE': [],
+            'DELETE': []
+        }
+
+        url = reverse('content-bulk', kwargs={'name': self.layer.name})
+        response = self.client.post(url, data, format='json')
+        # print(response.json())
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Tree.objects.last().code, 2)
 
     def tearDown(self):
-        sqls = ['DROP TABLE tree;', 'DROP SEQUENCE tree_code_seq']
         with self.conn.get_connection().cursor() as cursor:
-            for sql in sqls:
+            for sql in self.sql_to_delete:
                 cursor.execute(sql)
         super().tearDown()
