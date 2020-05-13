5999905e29c8a60219cf5ebcbef8d5c9d050b50d
abusquets <abusquets@gmail.com>
2020-05-13 07:16:03 +0200

    Hotfix giscube_search settings

--- giscube_search/settings.py
+++ giscube_search/settings.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 GISCUBE_SEARCH_DEFAULT_DICTIONARY = getattr(settings, 'GISCUBE_SEARCH_DEFAULT_DICTIONARY', 'english')
-GISCUBE_SEARCH_DEFAULT_DICTIONARY = getattr(settings, 'GISCUBE_SEARCH_MAX_RESULTS', None)
+GISCUBE_SEARCH_MAX_RESULTS = getattr(settings, 'GISCUBE_SEARCH_MAX_RESULTS', None)
