8d833bac241f36c0085c63f1515d6816999fba85
abusquets <abusquets@gmail.com>
2020-08-24 12:00:37 +0200

    Add bbox help text

--- giscube/model_mixins.py
+++ giscube/model_mixins.py
@@ -17,7 +17,9 @@ class MetadataModelMixin(models.Model):
     provider_web = models.URLField(_('provider web'), max_length=255, blank=True, null=True)
     provider_email = models.CharField(_('provider email'), max_length=255, blank=True, null=True)
     summary = models.TextField(_('summary'), blank=True, null=True)
-    bbox = models.CharField(_('BBOX'), max_length=255, blank=True, null=True)
+    bbox = models.CharField(_('BBOX'), max_length=255, blank=True, null=True,
+    help_text='The format is: xmin,ymin,xmin,xmax. BBOX coordinates must be in EPSG:4326'
+    )
 
     def __str__(self):
         return textwrap.shorten(text=self.information or '', width=50, placeholder='...')
