c64b71ce220298ed1616b7e153860e14147d6171
abusquets <abusquets@gmail.com>
2020-10-16 11:19:24 +0200

    Add url to resource files

--- giscube/utils/url.py
+++ giscube/utils/url.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 
 
+def full_url(url):
+    return url_slash_join(settings.SITE_URL, settings.APP_URL, url)
+
+
 def remove_app_url(path):
     if settings.APP_URL != '':
         if path.startswith(settings.APP_URL):
@@ -9,4 +13,5 @@ def remove_app_url(path):
 
 
 def url_slash_join(*args):
+    args = list(filter(None, args))
     return '/'.join([(u.strip('/') if index + 1 < len(args) else u.lstrip('/')) for index, u in enumerate(args)])
--- tests/test_url_utils.py
+++ tests/test_url_utils.py
@@ -27,6 +27,14 @@ class URLUtilsTestCase(BaseTest):
         url_ok = 'http://localhost/apps/giscube/imageserver/services/streets/'
         self.assertEqual(url, url_ok)
 
+        url = url_slash_join('http://localhost/', '/apps/giscube/', '/imageserver/services/streets')
+        url_ok = 'http://localhost/apps/giscube/imageserver/services/streets'
+        self.assertEqual(url, url_ok)
+
+        url = url_slash_join('http://localhost/', '', '/apps/giscube/', '/imageserver/services/streets')
+        url_ok = 'http://localhost/apps/giscube/imageserver/services/streets'
+        self.assertEqual(url, url_ok)
+
     def test_remove_app_url(self):
         url = remove_app_url('/apps/giscube/imageserver/services/streets')
         self.assertEqual(url, 'imageserver/services/streets')
