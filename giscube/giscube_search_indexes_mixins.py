341abf3cef957bc830b6fb2df6e715dfb8141497
abusquets <abusquets@gmail.com>
2020-10-16 11:19:24 +0200

    Add url to resource files

--- giscube/giscube_search_indexes_mixins.py
+++ giscube/giscube_search_indexes_mixins.py
@@ -1,12 +1,19 @@
+from .utils import full_url
+from . model_mixins import RESOURCE_TYPE_CHOICES
+
+
 class ResourcesIndexMixin:
     def prepare_children(self, obj):
         children = []
         for r in obj.resources.all():
+            url = r.url
+            if r.type == RESOURCE_TYPE_CHOICES.document and r.file is not None:
+                url = full_url(r.file.url)
             children.append({
                 'title': r.title or r.name,
                 'group': False,
                 'type': r.type,
-                'url': r.url,
+                'url': url,
                 'layers': r.layers,
                 'projection': r.projection,
                 'giscube': {
