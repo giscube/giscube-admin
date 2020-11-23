4c6c2de81f1dacdb951010bb60118621fcc3c096
Alexandre <abusquets@gmail.com>
2020-11-23 13:53:54 +0100

    Add single_image to services

--- giscube/tilecache/admin_forms_mixins.py
+++ giscube/tilecache/admin_forms_mixins.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from django.utils.translation import gettext as _
 
@@ -5,3 +6,14 @@ from django.utils.translation import gettext as _
 class TileCacheChangeFormMixin(forms.ModelForm):
     get_bbox_from_project = forms.BooleanField(label=_('Get BBOX from project'), required=False, initial=False)
     tilecache_clear_cache = forms.BooleanField(label=_('Clear cache'), required=False, initial=False)
+
+    def clean(self):
+        super().clean()
+        wms_buffer_enabled = self.cleaned_data['wms_buffer_enabled']
+        tilecache_enabled = self.cleaned_data['tilecache_enabled']
+        wms_single_image = self.cleaned_data['wms_single_image']
+
+        if (wms_buffer_enabled or tilecache_enabled) and wms_single_image:
+            msg = _("Is no possible have enabled 'WMS buffer enabled' or 'tilecache enabled'"
+                    "with 'WMS single image' at the same time")
+            self.add_error('wms_single_image', msg)
