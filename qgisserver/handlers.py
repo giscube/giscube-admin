09b2ae0f7650784fc03399e8a073108ac69ba749
Manel Clos <manelclos@gmail.com>
2021-01-14 17:53:12 +0100

    signals fixup

--- qgisserver/handlers.py
+++ qgisserver/handlers.py
@@ -5,10 +5,10 @@ from .utils import patch_qgis_project, update_external_service
 
 
 @receiver(service_updated)
-def service_updated_handler(sender, obj):
-    update_external_service(obj)
+def service_updated_handler(sender, obj, **kwargs):
+    update_external_service(obj.pk)
 
 
 @receiver(service_project_updated)
-def service_project_updated_handler(sender, obj):
+def service_project_updated_handler(sender, obj, **kwargs):
     patch_qgis_project(obj)
