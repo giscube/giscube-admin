77541e916771929ecfbb9bf6a0309ccdd0cd148d
Manel Clos <manelclos@gmail.com>
2021-01-15 12:12:40 +0100

    add 'application/x-zip-compressed' mime-type

--- qgisserver/admin_forms.py
+++ qgisserver/admin_forms.py
@@ -23,13 +23,22 @@ class ServiceChangeForm(TileCacheChangeFormMixin, forms.ModelForm):
             self.add_error('wms_single_image', msg)
 
         project = self.cleaned_data['project']
-        if project and project.content_type not in ('application/x-qgis-project', 'application/zip'):
+
+        if project and project.content_type not in (
+            'application/x-qgis-project',
+            'application/zip',
+            'application/x-zip-compressed',
+            'application/octet-stream',
+        ):
             raise forms.ValidationError(_('Only zip or qgs files are suported.'))
+
         if project and project.content_type == 'application/zip' and not zipfile.is_zipfile(project.file):
             raise forms.ValidationError(_('Invalid zipfile.'))
+
         if project and project.content_type == 'application/zip':
             zip_file = zipfile.ZipFile(project)
             name = '%s.qgs' % self.cleaned_data['name']
+
             if name not in zip_file.namelist():
                 raise forms.ValidationError(_('There is not %s in %s' % (name, self.cleaned_data['project'].name)))
 
