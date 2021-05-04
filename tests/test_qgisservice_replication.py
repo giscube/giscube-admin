e71775fb26c80b2f4ee4c90a5a04ee793193f33f
Alexandre Busquets Triola <abusquets@gmail.com>
2021-05-04 07:11:33 +0200

    wip

--- tests/test_qgisservice_replication.py
+++ tests/test_qgisservice_replication.py
@@ -3,12 +3,13 @@ import json
 
 from pathlib import Path
 
+from django.contrib.auth.models import User
 from django.core.files import File
 from django.urls import reverse
 
 from rest_framework import status
 
-from qgisserver.models import Service
+from qgisserver.models import Service, ServiceUserPermission
 from tests.common import BaseTest
 
 
@@ -63,7 +64,7 @@ class QGISServiceReplicationTestCase(BaseTest):
         self.assertTrue(result['group_permissions'][0]['can_view'])
         self.assertTrue(result['group_permissions'][0]['can_write'])
 
-    def test_update(self):
+    def test_update_status_ok(self):
         with open('tests/files/project1.qgs', 'rb') as f:
             Service.objects.create(
                 name='project-1',
@@ -103,3 +104,75 @@ class QGISServiceReplicationTestCase(BaseTest):
             }
             response = self.client.put(url, data=json.dumps(data), content_type='application/json')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_update_project(self):
+        # TODO:
+        self.assertTrue(False)
+
+    def test_update_users(self):
+        with open('tests/files/project1.qgs', 'rb') as f:
+            service = Service.objects.create(
+                name='project-1',
+                active=False,
+                project_file=File(f, name='project1.qgs')
+            )
+            for i in range(3):
+                user = User.objects.create(
+                    username=f'user{i}',
+                    email=f'user{i}@mail.com'
+                )
+                ServiceUserPermission.objects.create(
+                    layer=service,
+                    user=user,
+                    can_view=True,
+                    can_write=True
+                )
+
+        self.login_superuser()
+        url = reverse('qgisserver_service-list')
+        project_file_path = 'tests/files/project2.qgs'
+        file_name = Path(project_file_path).name
+        with open(project_file_path, 'rb') as project_file:
+            project_content = base64.b64encode(project_file.read()).decode('utf-8')
+            data = {
+                'name': 'project-1',
+                'active': True,
+                'user_permissions': [
+                    {
+                        'user': {
+                            'username': 'user2',
+                            'email': 'user2@mail.net'
+                        },
+                        'can_view': False,
+                        'can_write': False
+                    },
+                    {
+                        'user': {
+                            'username': 'user3',
+                            'email': 'user3@mail.net'
+                        },
+                        'can_view': False,
+                        'can_write': False
+                    }
+                ],
+                'group_permissions': [],
+                'project_file': f'data:text/xml;charset=utf8-8;name={file_name};base64,{project_content}'
+            }
+            response = self.client.put(url, data=json.dumps(data), content_type='application/json')
+        result = response.json()
+        self.assertEqual(len(result['user_permissions']), 2)
+        users = {}
+        for permission in result['user_permissions']:
+            users[permission['user']['username']] = permission
+
+        self.assertTrue(users['user2']['user']['email'], 'user2@mail.net')
+        self.assertTrue(users['user3']['user']['email'], 'user3@mail.net')
+
+        self.assertFalse(users['user2']['can_view'])
+        self.assertFalse(users['user2']['can_write'])
+        self.assertFalse(users['user3']['can_view'])
+        self.assertFalse(users['user3']['can_write'])
+
+    def test_update_groups(self):
+        # TODO:
+        self.assertTrue(False)
