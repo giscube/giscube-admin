56f7a7982a1bc72fed134d6763e1529f39c68136
Marc Compte <marc@compte.me>
2021-07-23 18:14:11 +0200

    Map configuration (#635)
    * Create the map configuration models.
    * Define an endpoint to return the MapConfig data.
    * Add the admin view.

--- giscube/serializers.py
+++ giscube/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 
-from .models import Category, UserAsset
+from .models import Category, MapConfig, MapConfigBaseLayer, UserAsset
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -29,3 +29,21 @@ class UserAssetSerializer(serializers.ModelSerializer):
         model = UserAsset
         fields = ('uuid', 'file', 'created')
         read_only_fields = ('uuid', 'created')
+
+
+class MapConfigBaseLayerSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='base_layer.id')
+    name = serializers.ReadOnlyField(source='base_layer.name')
+    properties = serializers.ReadOnlyField(source='base_layer.properties')
+
+    class Meta:
+        model = MapConfigBaseLayer
+        fields = ('id', 'order', 'name', 'properties')
+
+
+class MapConfigSerializer(serializers.ModelSerializer):
+    baselayers = MapConfigBaseLayerSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = MapConfig
+        fields = ('id', 'name', 'center_lat', 'center_lng', 'initial_zoom', 'baselayers')
