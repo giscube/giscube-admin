672171dd4e0278672b1b1addea37da3c7053f5b1
Manel Clos <manelclos@gmail.com>
2021-07-23 17:37:26 +0200

    fixup migration

--- qgisserver/migrations/0027_auto_20210610_1311.py
+++ qgisserver/migrations/0027_auto_20210610_1311.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('qgisserver', '0025_auto_20201127_1450'),
+        ('qgisserver', '0026_service_wms_getfeatureinfo_enabled'),
     ]
 
     operations = [
