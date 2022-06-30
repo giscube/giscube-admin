48135f4379fd570f6b6aff970daed8b85d9b4d3f
JordiGomara <jg@microdisseny.com>
2022-06-30 13:13:15 +0200

    Geojson Layer with filters
    Fix import geojson filter serializer

--- layerserver/serializers/__init__.py
+++ layerserver/serializers/__init__.py
@@ -3,6 +3,7 @@ from .databaselayer import (DBLayerDetailSerializer, DBLayerReferenceSerializer,
 from .databaselayer_content import create_dblayer_serializer
 from .dblayer_field import DBLayerFieldListSerializer, DBLayerFieldSerializer
 from .dblayer_virtualfield import DBLayerVirtualFieldListSerializer, DBLayerVirtualFieldSerializer
+from .geojsonfilter import GeoJSONFilterSerializer
 from .geojsonlayer import GeoJSONLayerSerializer
 from .geojsonlayerlog import GeoJSONLayerLogSerializer
 
@@ -10,7 +11,7 @@ from .geojsonlayerlog import GeoJSONLayerLogSerializer
 __all__ = [
     'DBLayerFieldListSerializer', 'DBLayerFieldSerializer',
     'DBLayerVirtualFieldListSerializer', 'DBLayerVirtualFieldSerializer',
-    'GeoJSONLayerSerializer', 'GeoJSONLayerLogSerializer',
+    'GeoJSONLayerSerializer', 'GeoJSONLayerLogSerializer', 'GeoJSONFilterSerializer',
     'DBLayerSerializer', 'DBLayerReferenceSerializer', 'DBLayerDetailSerializer', 'style_representation',
     'style_rules_representation',
     'create_dblayer_serializer',
