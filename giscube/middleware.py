18ae5559ae8dccbcf4799bcf7eb432c80e658fdf
aroiginfraplan <59873981+aroiginfraplan@users.noreply.github.com>
2023-02-07 14:04:22 +0100

    Add parse uri to middleware

--- giscube/middleware.py
+++ giscube/middleware.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse, parse_qs
+
 from oauth2_provider.middleware import OAuth2TokenMiddleware
 
 
@@ -6,10 +8,15 @@ class AccesTokenOAuth2TokenMiddleware(OAuth2TokenMiddleware):
         self.get_response = get_response
 
     def __call__(self, request):
-        if access_token := request.GET.get('access_token'):
-            request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
-            return super().__call__(request)
+        if not request.META.get('HTTP_AUTHORIZATION', '').startswith('Bearer') and request.GET.get('access_token'):
+            bearer = 'Bearer %s' % request.GET.get('access_token')
+            request.META['HTTP_AUTHORIZATION'] = bearer
 
-        response = self.get_response(request)
+        if uri := request.META.get('REQUEST_URI'):
+            url_parse = urlparse(uri)
+            query_string = parse_qs(url_parse.query)
+            if 'access_token' in query_string:
+                bearer = 'Bearer %s' % query_string['access_token'][0]
+                request.META['HTTP_AUTHORIZATION'] = bearer
 
-        return response
+        return super().__call__(request)
