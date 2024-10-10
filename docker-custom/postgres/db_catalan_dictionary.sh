f10eb8a36c8f6a409a1406b247cdfd062ba994fb
Jordi Gomara Cruz <jg@microdisseny.com>
2024-10-10 12:06:49 +0200

    fix postgres version

--- docker-custom/postgres/db_catalan_dictionary.sh
+++ docker-custom/postgres/db_catalan_dictionary.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cp /app/docker-custom/postgres/dictionaries/dist/ca.affix /usr/share/postgresql/11/tsearch_data/
-cp /app/docker-custom/postgres/dictionaries/dist/ca.dict /usr/share/postgresql/11/tsearch_data/
-cp /app/docker-custom/postgres/dictionaries/dist/catalan.stop /usr/share/postgresql/11/tsearch_data/
+cp /app/docker-custom/postgres/dictionaries/dist/ca.affix /usr/share/postgresql/12/tsearch_data/
+cp /app/docker-custom/postgres/dictionaries/dist/ca.dict /usr/share/postgresql/12/tsearch_data/
+cp /app/docker-custom/postgres/dictionaries/dist/catalan.stop /usr/share/postgresql/12/tsearch_data/
 
 PGPASSWORD="${DB_PASSWORD}" psql -hlocalhost -U${DB_USER} --set ON_ERROR_STOP=on ${DB_NAME} < /app/docker-custom/postgres/dictionaries/catalan.sql
