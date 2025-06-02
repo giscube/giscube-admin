547ba7e19c91cfddcd676e31d1fe13b0c0385952
JordiGomara <jg@microdisseny.com>
2025-06-02 17:11:23 +0200

    color and icon

--- layerserver/serializers/geojsonfilter.py
+++ layerserver/serializers/geojsonfilter.py
@@ -4,6 +4,15 @@ from layerserver.models import GeoJsonFilter
 
 
 class GeoJSONFilterSerializer(serializers.ModelSerializer):
+    color = serializers.CharField(required=False)
+    icon = serializers.CharField(required=False)
+
     class Meta:
         model = GeoJsonFilter
-        fields = ['title', 'description', 'filter']
+        fields = ['title', 'description', 'filter', 'color', 'icon']
+
+    def get_color(self, obj):
+        return obj.fill_color if obj.fill_color else obj.stroke_color or obj.icon_color
+    
+    def get_icon(self, obj):
+        return obj.icon
