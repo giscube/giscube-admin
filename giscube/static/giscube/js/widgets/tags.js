0d52d87c705ed5a79e66d8ea6df2de65cb7f808e
aroiginfraplan <59873981+aroiginfraplan@users.noreply.github.com>
2025-06-02 18:10:07 +0200

    Public function initGiscubeTagify

--- giscube/static/giscube/js/widgets/tags.js
+++ giscube/static/giscube/js/widgets/tags.js
@@ -1,56 +1,58 @@
+function initGiscubeTagify () {
+  const $ = django.jQuery;
+  const selectorSortable = 'tags.tags-widget';
+
+  var $element = $(this);
+
+  $element.hide();
+  var $target = $('<input type="text" class="tags-widget">');
+  $target.val($element.val());
+  $($element).before($target);
+
+  var tagify;
+  function update_element () {
+      var value = '', values = [];
+      try {
+        values = JSON.parse($target.val());
+    } catch (err) {}
+    for (var x in values){
+        if (x > 0){
+          value += ',';
+        }
+        value += values[x].value;
+    }
+    $element.val(value);
+  }
+
+  tagify = new Tagify($target[0], {
+    dropdown: {enabled: 0},
+    callbacks: {add: update_element, remove: update_element}
+  });
+  $element.data('tagify', tagify);
+
+  function onEnd(evt) {
+    var item = tagify.value[evt.oldIndex];
+    // Remove old index
+    tagify.value.splice(evt.oldIndex, 1)
+    var newIndex = evt.newIndex;
+    // Insert item into new position
+    tagify.value.splice(newIndex, 0, item);
+
+    tagify.update();
+    update_element();
+  }
+
+  Sortable.create($element.parent().find(selectorSortable)[0], {
+    draggable: 'tag',
+    onEnd: onEnd
+  });
+};
+
 (function ($) {
   'use strict';
   const selector = 'textarea.tags-widget';
-  const selectorSortable = 'tags.tags-widget';
-
-  function init() {
-    var $element = $(this);
-    $element.hide();
-    var $target = $('<input type="text" class="tags-widget">');
-    $target.val($element.val());
-    $($element).before($target);
-
-    var tagify;
-    function update_element () {
-        var value = '', values = [];
-        try {
-          values = JSON.parse($target.val());
-      } catch (err) {}
-      for (var x in values){
-          if (x > 0){
-            value += ',';
-          }
-          value += values[x].value;
-      }
-      $element.val(value);
-    }
-
-    tagify = new Tagify($target[0], {
-      dropdown: {enabled: 0},
-      callbacks: {add: update_element, remove: update_element}
-    });
-    $element.data('tagify', tagify);
-
-    function onEnd(evt) {
-      var item = tagify.value[evt.oldIndex];
-      // Remove old index
-      tagify.value.splice(evt.oldIndex, 1)
-      var newIndex = evt.newIndex;
-      // Insert item into new position
-      tagify.value.splice(newIndex, 0, item);
-
-      tagify.update();
-      update_element();
-    }
-
-    Sortable.create($element.parent().find(selectorSortable)[0], {
-      draggable: 'tag',
-      onEnd: onEnd
-    });
-  };
-
   $(function () {
-    $(selector).each(init);
+    $(selector).each(initGiscubeTagify);
   });
 
 })(django.jQuery);
