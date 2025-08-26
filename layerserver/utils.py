738d08fbc1e6e07bf9554f793b038d0114b3770f
Marc Compte <marc@braquets.cat>
2025-08-26 12:33:44 +0200

    02025 | Mapia SERVER: veure tasques en cua

--- layerserver/utils.py
+++ layerserver/utils.py
@@ -63,7 +63,7 @@ def geojsonlayer_check_cache(layer):
             return GENERATE_GEOJSON_LAYER_PENDING
 
         from .tasks import async_geojsonlayer_refresh
-        async_geojsonlayer_refresh.delay(layer.pk, True)
+        async_geojsonlayer_refresh.delay(layer.pk, True, False, layer_name=layer.name)
         return QUEUE_GENERATE_GEOJSON_LAYER
 
 
