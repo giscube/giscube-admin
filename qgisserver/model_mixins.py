e086bcbc3efffb0dbffb98525b9c6a097df5cbad
Marc Compte <marc@braquets.cat>
2025-11-10 12:55:39 +0100

    Retocs servei WFS

--- qgisserver/model_mixins.py
+++ qgisserver/model_mixins.py
@@ -1,12 +1,11 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class WFSMixin(models.Model):
 
     wfs_enabled = models.BooleanField(_('enabled'), default=False)
-    wfs_transaction_enabled = models.BooleanField(_('\'Transaction\' requests enabled'), null=False, default=True)
+    wfs_transaction_enabled = models.BooleanField(_('Transaction requests enabled'), null=False, default=True)
 
     class Meta:
         abstract = True
