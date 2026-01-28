e7a0bebf7108fc743a1f28d691ca67a8a294f464
Jordi Gomara Cruz <jg@microdisseny.com>
2026-01-28 17:32:10 +0100

    02273 | Afegir últim login Geoportal

--- giscube/urls.py
+++ giscube/urls.py
@@ -21,7 +21,7 @@ router.register(r'giscube/user-assets', api.UserAssetViewSet, basename='user_ass
 # OAuth2 provider endpoints
 oauth2_endpoint_views = [
     path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path('token/', oauth2_views.TokenView.as_view(), name="token"),
+    path('token/', views.CustomTokenView.as_view(), name="token"),
     path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
 ]
 
