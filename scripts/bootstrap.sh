a83ca57a927e4470a0d8c3e895597b15c01f8793
idmicrodisseny <id@microdisseny.com>
2026-09-03 14:53:51 +0200

    fixtures

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,39 @@
 docker compose exec django python3 manage.py migrate
 
 # Admin user
-docker compose exec django python3 manage.py shell -c "from django.contrib.auth.models import User;User.objects.create_superuser('admin', 'tech@microdisseny.com', 'admin')"
+docker compose exec django python3 manage.py shell -c "from django.contrib.auth.models import User;User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+#"Giscube - Configuració general i Geoportal:"
+docker compose exec django python3 manage.py loaddata \
+  Group.json \
+  Users.json \
+  Category.json \
+  MetadataCategory.json \
+  MapConfig.json \
+  BaseLayer.json \
+  DBConnection.json \
+  Server.json
+
+docker compose exec django python3 manage.py loaddata \
+  MapConfigBaseLayer.json \
+  Dataset.json \
+  DatasetGroupPermission.json \
+  DatasetUserPermission.json
+
+#"Layerserver - Layer Manager:"
+docker compose exec django python3 manage.py loaddata \
+  GeoJsonLayer.json \
+  GeoJsonLayerGroupPermission.json \
+  GeoJsonLayerUserPermission.json \
+  GeoJsonLayerMetadata.json \
+  GeoJsonLayerStyleRule.json
+
+docker compose exec django python3 manage.py loaddata \
+  DataBaseLayer.json \
+  DataBaseLayerField.json \
+  DataBaseLayerStyleRule.json \
+  DataBaseLayerMetadata.json \
+  DBLayerGroup.json \
+  DBLayerUser.json
+
+docker compose exec django python3 manage.py loaddata \
+  Application.json
